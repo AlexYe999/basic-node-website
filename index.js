0cfe97d12fe7b06b63758bb18232d56e48e4deb0
your name <you@example.com>
2025-01-09 22:12:37 +1100

    finished task of adding pages to a server, very poor front end design!

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "./pages/";
+    if (q.pathname == "/") filename += "index.html";
+    else if (q.pathname == "/about") filename += "about.html";
+    else if (q.pathname == "/contact-me") filename += "contact-me.html";
+    else filename += "404.html";
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write();
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
