154cb991de2fc158f9d4698c1d465a36c5ce1405
your name <you@example.com>
2025-01-12 14:52:01 +1100

    fixed case with unknown URLs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/about", (req, res) => res.sendFile(__dirname + "/pages/about.html"));
 app.get("/contact-me", (req, res) =>
   res.sendFile(__dirname + "/pages/contact-me.html")
 );
-app.all("*", (req, res) => res.sendFile(__dirname + "/pages/404.html"));
+app.get("*", (req, res) => res.sendFile(__dirname + "/pages/404.html"));
 
 const PORT = 3000;
 app.listen(PORT, () => {
